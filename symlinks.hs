d00ca6f7f297860fd55bdca7e6df236cae7f9dae
Gary A. Howard <gahoward@amgen.com>
2015-01-22 19:58:02 -0800

    Symbolic link supported added to dotfiles.

---
+++ symlinks.hs
@@ -0,0 +1,34 @@
+-- | Copyright (c) Gary Allan Howard aka Traap.
+-- Distributed under the same terms as Vim itself.  See :help license.
+--
+-- This program initializes my HOME directory with symbolic link references to
+-- my git/dotfiles folders.
+module Main (main) where
+
+import System.Directory
+import System.Exit
+import System.Process
+
+-- | Create a symbolic link for the following files.
+dotfiles = 
+  ["bash_profile"
+  ,"gitconfig"
+  ,"inputrc"
+  ,"vim"
+  ,"vimrc"
+  ]
+
+-- | Let's move some files around.
+main :: IO ()
+main = do 
+  mapM_ makeSymbolicLink dotfiles
+
+-- | makeSymoblicLink
+makeSymbolicLink :: String -> IO ExitCode 
+makeSymbolicLink f = do
+  c <- getCurrentDirectory
+  h <- getHomeDirectory
+  let tfile = h ++ "/." ++ f
+  let sfile = c ++ "/" ++ f
+  system $ "rm -rf " ++ tfile
+  system $ "ln -vs " ++ sfile ++ " " ++ tfile
