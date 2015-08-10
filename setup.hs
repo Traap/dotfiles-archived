36a0f95bc1345209367cdd88df5e275b030fa784
Gary A. Howard <gahoward@amgen.com>
2015-08-10 15:15:29 -0700

    Add ~/.ssh/config to my personalization.

--- setup.hs
+++ setup.hs
@@ -5,13 +5,19 @@
 -- my git/dotfiles folders.
 module Main (main) where
 
+import Data.List.Split
 import System.Directory
 import System.Exit
 import System.Process
 
+-- | Return the last word of a string.
+lastWord :: String -> String
+lastWord = last . splitOn "/"
+
 -- | Create a symbolic link for the following files.
 dotfiles =
-  ["bash_profile"
+  ["ssh/config"
+  ,"bash_profile"
   ,"bashrc"
   ,"gitconfig"
   ,"gitignore_global"
@@ -36,7 +42,7 @@ makeSymbolicLink f = do
   system $ "rm -rf " ++ tfile
   
   c <- getCurrentDirectory
-  let sfile = c ++ "/" ++ f
+  let sfile = c ++ "/" ++ (lastWord f)
   system $ "ln -vs " ++ sfile ++ " " ++ tfile
 
 -- | run my getbundles.hs program
