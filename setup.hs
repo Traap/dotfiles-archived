4ea95c147b07a59f5068d9ae03620056d5ebfcd4
Gary A. Howard <gahoward@amgen.com>
2015-08-24 09:08:53 -0700

    Typographical correction.

--- setup.hs
+++ setup.hs
@@ -46,7 +46,7 @@ bundles s x =
   [s ++ "bling/vim-airline"
   ,s ++ "chriskempson/base16-vim"
   ,s ++ "christoomey/vim-tmux-navigator"
-  ,s ++ "christmomey/vim-tmux-runner"
+  ,s ++ "christoomey/vim-tmux-runner"
   ,s ++ "edkolev/tmuxline.vim"
   ,s ++ "moll/vim-bbye"
   ,s ++ "neovimhaskell/haskell-vim"
