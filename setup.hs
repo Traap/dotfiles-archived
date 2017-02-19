6c4790f85c37ba6535aa1534bd4dd67f4c5c0539
Traap <gary.a.howard@mac.com>
2017-02-18 19:10:03 -0800

    Issue #2 - Modify SymLink data type.

--- setup.hs
+++ setup.hs
@@ -6,6 +6,7 @@
 -- directories that are under version control, and 2) clone GitHub.com
 -- repositories that are needed to personalize bash and vim.
 
+-- -----------------------------------------------------------------------------
 -- | The main entry point.
 module Main (main) where
 
@@ -15,43 +16,51 @@ import System.Exit
 import System.FilePath
 import System.Process
 
+-- -----------------------------------------------------------------------------
 -- | A string representing git clone and Github URL.
 -- Github is perpended to all git repos in bundles.
 github :: String
 github = "git clone http://github.com/"
 
+-- -----------------------------------------------------------------------------
 -- | The SymLink type defines the target of a symlink operation and identifies
 -- the target type as a file or as a directory.
 data SymLink = SYMLINK
   {sym  :: String -- the target for the Symlink.
+  ,src  :: String -- the source of the Symlink.
   ,flag :: Bool -- true when a file; false when a directory.
   } deriving (Show)
 
+-- -----------------------------------------------------------------------------
 -- | The symlinks function creates a list of SymLinks.  Each SymLink represents
 -- a named symbolic link and file or directory target that is symbolically
 -- linked.
 symlinks :: [SymLink]
 symlinks =
-  [SYMLINK{sym="bash_profile",     flag=True}
-  ,SYMLINK{sym="bashrc",           flag=True}
-  ,SYMLINK{sym="gitconfig",        flag=True}
-  ,SYMLINK{sym="gitignore_global", flag=True}
-  ,SYMLINK{sym="gvimrc",           flag=True}
-  ,SYMLINK{sym="inputrc",          flag=True}
-  ,SYMLINK{sym="ssh",              flag=False}
-  ,SYMLINK{sym="ssh/config",       flag=True}
-  ,SYMLINK{sym="tmux",             flag=False}
-  ,SYMLINK{sym="tmux.conf",        flag=True}
-  ,SYMLINK{sym="vim",              flag=False}
-  ,SYMLINK{sym="vimrc",            flag=True}
-  ,SYMLINK{sym="vimrc_background", flag=True}
+  [SYMLINK{sym="bash_profile",     src=".",           flag=True}
+  ,SYMLINK{sym="bashrc",           src=".",           flag=True}
+  ,SYMLINK{sym="gitconfig",        src=".",           flag=True}
+  ,SYMLINK{sym="gitignore_global", src=".",           flag=True}
+  ,SYMLINK{sym="gvimrc",           src=".",           flag=True}
+  ,SYMLINK{sym="inputrc",          src=".",           flag=True}
+  ,SYMLINK{sym="ssh",              src=".",           flag=False}
+  ,SYMLINK{sym="config",           src=".ssh/config", flag=True}
+  ,SYMLINK{sym="tmux",             src=".",           flag=False}
+  ,SYMLINK{sym="tmux.conf",        src=".",           flag=True}
+  ,SYMLINK{sym="vim",              src=".",           flag=False}
+  ,SYMLINK{sym="vimrc",            src=".",           flag=True}
+  ,SYMLINK{sym="vimrc_background", src=".",           flag=True}
   ]
 
+-- -----------------------------------------------------------------------------
+-- | The Url type defines a Url and whether or not the url is cloned to . (here)
+-- or a new directory.
 data Url = URL
-  { loc  :: String -- the url string.
-  , here :: Bool   -- use . (here) versus url name to clone into.
+  {loc  :: String -- the url string.
+  ,here :: Bool   -- use . (here) versus url name to clone into.
   } deriving (Show)
 
+-- -----------------------------------------------------------------------------
 -- | The Repo type defines target directory for a git-clone operation and the
 -- URL cloned from GitHub.com.
 data Repo = REPO
@@ -59,6 +68,7 @@ data Repo = REPO
   ,url  :: [Url] -- the repositories to clone.
   } deriving (Show)
 
+-- -----------------------------------------------------------------------------
 -- | The repos function creates a list of Repos.  Each Repo represents
 -- a repository cloned from GitHub.com.
 repos :: String -> [Repo]
@@ -80,49 +90,45 @@ repos s =
        }
   ]
 
+-- -----------------------------------------------------------------------------
 -- | Orchestrate cloning GitHub.com repositories and creating symbolic links.
+-- Step 1: Remove everything we are about to create.
+-- Step 2: Clone repositories from github.
+-- Step 3: Create symbolic links.
 main :: IO ()
 main = do
-  -- Step 1: Remove everything we are about to create. 
   mapM_ deleteSymLink symlinks
-
-  -- Step 2: Clone repositories from github.
   mapM_ withDirCloneRepo $ repos github
-
-  -- Step 3: Create symbolic links.
   mapM_ makeSymLink symlinks
 
--- | Recursively delete objects referenced by SymLink. 
+-- -----------------------------------------------------------------------------
+-- | Recursively delete objects referenced by SymLink.
 deleteSymLink :: SymLink -> IO ExitCode
 deleteSymLink sl = do
-  -- Concatenate target file name (ex: ~/.bashrc).
   h <- getHomeDirectory
-  let tfile = h ++ "/." ++ sym sl
+  let tfile = h ++ "/" ++ src sl ++ sym sl
+  system $ "rm -vrf " ++ tfile 
 
-  -- Remove the target file and create the symbolic link.
-  system $ "rm -vrf " ++ tfile
-
--- | Create symbolic links for objects referenced by SymLink. 
+-- -----------------------------------------------------------------------------
+-- | Create symbolic links for objects referenced by SymLink.
+-- Concatenate source file name (ex: ~/git/dotfiles/bashrc).
 makeSymLink :: SymLink -> IO ExitCode
 makeSymLink sl = do
-  -- Concatenate target file name (ex: ~/.bashrc).
   h <- getHomeDirectory
-  let tfile = h ++ "/." ++ sym sl
+  let tfile = h ++ "/" ++ src sl ++ sym sl
 
-  -- Concatenate source file name (ex: ~/git/dotfiles/bashrc).
   c <- getCurrentDirectory
   let sfile = c ++ "/" ++ takeFileName (sym sl)
+  system $ "ln -vs " ++ sfile ++ " " ++ tfile 
 
-  -- Remove the target file and create the symbolic link.
-  _ <- system $ "rm -vrf " ++ tfile
-  system $ "ln -vs " ++ sfile ++ " " ++ tfile
-
+-- -----------------------------------------------------------------------------
 -- | Setup directory to clone repository into.
 withDirCloneRepo :: Repo -> IO ()
 withDirCloneRepo r = do
   setupDirectory (tdir r)
   mapM_ cloneRepo (url r)
 
+-- -----------------------------------------------------------------------------
 -- | Setup directory.
 setupDirectory :: FilePath -> IO ()
 setupDirectory fpath = do
@@ -131,6 +137,7 @@ setupDirectory fpath = do
   createDirectoryIfMissing True fpath
   setCurrentDirectory fpath
 
+-- -----------------------------------------------------------------------------
 -- | Set ~/dotfiles/vim directory
 setDotFileDirectory :: IO ()
 setDotFileDirectory = do
@@ -138,12 +145,14 @@ setDotFileDirectory = do
   let f = h ++ "/git/dotfiles"
   setCurrentDirectory f
 
+-- -----------------------------------------------------------------------------
 -- | Safely remove the directory and all sub-folders.
 safelyRemoveDirectory :: FilePath -> IO ()
 safelyRemoveDirectory fpath = do
   b <- doesDirectoryExist fpath
   Control.Monad.when b $ removeDirectoryRecursive fpath
 
+-- -----------------------------------------------------------------------------
 -- | Clone repos I am interested in using.
 -- The url cloned has two formats
 -- 1) clone git@github.com:Traap/dotfiles
